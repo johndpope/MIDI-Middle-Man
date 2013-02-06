6201dc5edccda43803baa1a68b00b5adbd350799
Joe Noël <joe.noel@focusrite.com>
2013-02-06 14:05:17 +0000

    Added MIDI functions

--- MIDI Middle Man/main.cpp
+++ MIDI Middle Man/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include "CoreMIDI/MIDIServices.h"
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << MIDIGetNumberOfDevices() << endl << endl;
+    
+    std::cout << MIDIGetDevice(1);
+    
+    MIDIInputPortCreate(<#MIDIClientRef client#>, <#CFStringRef portName#>, <#MIDIReadProc readProc#>, <#void *refCon#>, <#MIDIPortRef *outPort#>)
+    
+    OSStatus MIDIInputPortCreate (
+                                  MIDIClientRef  client,
+                                  CFStringRef    portName,
+                                  MIDIReadProc   readProc,
+                                  void           *refCon,
+                                  MIDIPortRef    *outPort
+                                  );
+    
+    
     return 0;
 }
+
+
 
