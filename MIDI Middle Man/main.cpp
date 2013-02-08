c6ba8f77351deccf25eeffc76179802c3f56965f
Joe Noël <joe.noel@focusrite.com>
2013-02-08 16:32:51 +0000

    Routes MIDI in from first source to MIDI out

--- MIDI Middle Man/main.cpp
+++ MIDI Middle Man/main.cpp
@@ -26,6 +26,7 @@ static void	MyReadProc(const MIDIPacketList *pktlist, void *refCon, void *connRe
 		}
         
 		MIDISend(gOutPort, gDest, pktlist);
+
 	}
 }
 
@@ -41,19 +42,21 @@ int main(int argc, const char * argv[])
 	MIDIPortRef inPort = NULL;
 	err = MIDIInputPortCreate(client, CFSTR("MIDI Middle Man - Input"), MyReadProc, NULL, &inPort);
 	err = MIDIOutputPortCreate(client, CFSTR("MIDI Middle Man - Output"), &gOutPort);
+        
+    // create source and destination
+    MIDIEndpointRef myDestination = NULL;
+    err = MIDISourceCreate(client, CFSTR("MIDI Middle Man - Output"), &gDest);
+    err = MIDIDestinationCreate(client, CFSTR("MIDI Middle Man - Input"), MyReadProc, NULL, &myDestination);
     
-    // create input endpoint
-    MIDIEndpointRef gSource = NULL;
+    // MIDIReceived(&gDest, pktlist);
     
-    // Create source and destination
-    err = MIDISourceCreate(client, CFSTR("MIDI Middle Man"), &gSource);
-    err = MIDIDestinationCreate(client, CFSTR("MIDI Middle Man"), MyReadProc, NULL, &gDest);
-    
-    // connect input endpoint to input port
-    gSource = MIDIGetSource(0); // Put source number in here
-    err = MIDIPortConnectSource(inPort, gSource, NULL);
-    
-    
+    // connect source to input port
+    gDest = MIDIGetDestination(0);
+    myDestination = MIDIGetSource(0); // Put source number in here
+    err = MIDIPortConnectSource(inPort, myDestination, NULL);
+    err = MIDIPortConnectSource(gOutPort, gDest, NULL);
+       
+    //
     // print the name, manufacturer and model for all connected devices
 	CFStringRef pname, pmanuf, pmodel;
 	char name[64], manuf[64], model[64];
@@ -75,7 +78,37 @@ int main(int argc, const char * argv[])
         
         
 		printf("number=%i name=%s, manuf=%s, model=%s\n", i, name, manuf, model);
+        
 	}
+    
+    
+    // print out list of sources
+    CFStringRef sourceName;
+    char sourceNameC[50];
+    ItemCount sources = MIDIGetNumberOfSources();
+    printf("\nNumber of sources: %i\n", sources);
+    for (int ppp = 0; ppp < sources; ++ppp)
+    {
+        MIDIEndpointRef endpoint = MIDIGetSource(ppp);
+        MIDIObjectGetStringProperty( endpoint, kMIDIPropertyName, &sourceName);
+        CFStringGetCString(sourceName, sourceNameC, sizeof(sourceNameC), 0);
+        printf("%i: %s\n", ppp, sourceNameC);
+    }
+    
+    // print out list of destinations
+    CFStringRef destinationName;
+    char destinationNameC[50];
+    ItemCount destinations = MIDIGetNumberOfSources();
+    printf("\nNumber of destinations: %i\n", destinations);
+    for (int ppp = 0; ppp < sources; ++ppp)
+    {
+        MIDIEndpointRef endpoint = MIDIGetDestination(ppp);
+        MIDIObjectGetStringProperty( endpoint, kMIDIPropertyName, &destinationName);
+        CFStringGetCString(destinationName, destinationNameC, sizeof(destinationNameC), 0);
+        printf("%i: %s\n", ppp, destinationNameC);
+    }
+    //
+    
     
     
     
