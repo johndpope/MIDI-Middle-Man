3c170d89dfda656d8c13e57090d83de96ea7244f
Joe Noël <joe.noel@focusrite.com>
2013-02-20 15:04:30 +0000

    Updated readprocs for scalability

--- MIDI Middle Man/main.cpp
+++ MIDI Middle Man/main.cpp
@@ -3,28 +3,42 @@
 #include <stdio.h>
 #import <Cocoa/Cocoa.h>
 
-#define DESIRED_SOURCE_NAME "Launchkey MIDI"
-#define DESIRED_DESTINATION_NAME "Launchkey MIDI"
+#define DESIRED_SOURCE_NAME "Launchpad S"
+#define DESIRED_DESTINATION_NAME "Launchpad S"
 
-MIDIEndpointRef     source, destination;
-MIDIEndpointRef     inputDev, outputDev;
-MIDIPortRef         inputPort, outputPort;
 ItemCount           gSources, gDestinations;
+NSMutableArray      *sourceList, *destinationList;
+
+struct OutputPortDevicePair
+{
+    MIDIPortRef outputPort;
+    MIDIEndpointRef outputDevice;
+};
 
 // when MIDI present at input port (should be connected to external source), distributes MIDI to source
 // should be passed source from MIDIInputPortCreate
 static void	InputReadProc(const MIDIPacketList *pktlist, void * refCon, void * connRefCon)
 {
-    UInt32 * sourceNumber;
-    sourceNumber = (UInt32 *) refCon;
-    MIDIEndpointRef source = * sourceNumber;
-	MIDIReceived(source, pktlist);
+    MIDIEndpointRef *mySource = (MIDIEndpointRef *) refCon;
+    
+	MIDIReceived(*mySource, pktlist); // distribute MIDI to source
 }
 
 // when MIDI present at destination, distributes MIDI from output port to chosen output devices
 static void	DestinationReadProc(const MIDIPacketList *pktlist, void *refCon, void *connRefCon)
 {
-    MIDISend(outputPort, outputDev, pktlist);
+    
+    
+    OutputPortDevicePair *outputPortDevicePair = (OutputPortDevicePair *) refCon;
+    MIDIPortRef outputPort = outputPortDevicePair->outputPort;
+    MIDIEndpointRef outputDevice = outputPortDevicePair->outputDevice;
+    
+    if (outputPort != NULL && outputDevice != NULL)
+    {
+        MIDISend(outputPort,outputDevice, pktlist); // distribute MIDI from outputPort to outputDev
+        // printf("Sending MIDI\n");
+    }
+    
 }
 
 // stores a list of current sources in sourceList and returns the number of sources
@@ -42,6 +56,7 @@ ItemCount ListCurrentSources(NSMutableArray* sourceList)
         [sourceList insertObject: pNSSourceName atIndex: nnn];
 
     }
+    printf("Created list of sources\n");
     return sources;
 }
 
@@ -60,6 +75,7 @@ ItemCount ListCurrentDestinations(NSMutableArray* destinationList)
         [destinationList insertObject: pNSDestinationName atIndex: nnn];
         
     }
+    printf("Created list of destinations\n");
     return destinations;
 }
 
@@ -68,6 +84,9 @@ ItemCount ListCurrentDestinations(NSMutableArray* destinationList)
 NSUInteger FindIndexOfDesiredSource(NSArray * sourceList, CFStringRef desiredSourceName)
 {
     NSString * pNSDesiredSourceName = (__bridge NSString *) desiredSourceName;
+    
+    int indexOfDesiredSource = [sourceList indexOfObject:pNSDesiredSourceName];
+    printf("Desired source at index: %i\n", indexOfDesiredSource);
     
     return [sourceList indexOfObject:pNSDesiredSourceName];
     
@@ -79,21 +98,38 @@ NSUInteger FindIndexOfDesiredDestination(NSArray * destinationList, CFStringRef 
 {
     NSString * pNSDesiredDestinationName = (__bridge NSString *) desiredDestinationName;
     
-    return [destinationList indexOfObject:pNSDesiredDestinationName];
+    int indexOfDesiredDestination = [destinationList indexOfObject:pNSDesiredDestinationName];
+    printf("Desired destination at index: %i\n", indexOfDesiredDestination);
     
+    return [destinationList indexOfObject:pNSDesiredDestinationName];
 }
 
 // connects a source at an index to the inputPort
-void ConnectInputs(NSUInteger indexOfDesiredSource, MIDIEndpointRef inputPort)
+void ConnectInputs(NSUInteger indexOfDesiredSource, MIDIPortRef inputPort)
 {     
+    if (indexOfDesiredSource != NSNotFound)
+    {
         MIDIEndpointRef inputDev = MIDIGetSource((int) indexOfDesiredSource); // more elegant cast here?
         MIDIPortConnectSource(inputPort, inputDev, NULL);
+    }
+    else
+    {
+        printf("Desired source not connected\n");
+    }
 }
 
-MIDIEndpointRef GetDeviceAtDestination(NSUInteger indexOfDesiredDestination)
+void ConnectOutputs(NSUInteger indexOfDesiredDestination, OutputPortDevicePair* outputPortDevicePair)
 {
-        MIDIEndpointRef outputDev = MIDIGetDestination(indexOfDesiredDestination);
-        return outputDev;
+    if (indexOfDesiredDestination != NSNotFound)
+    {
+        MIDIEndpointRef endpoint= MIDIGetDestination(indexOfDesiredDestination);
+        outputPortDevicePair->outputDevice = endpoint;
+    }
+    else
+    {
+        printf("Desired destination not connected\n");
+    }
+    
 }
 
 
@@ -139,26 +175,35 @@ void NotifyProc (const MIDINotification *message, void *refCon)
     if ( HaveSourcesChanged() || HaveDestinationsChanged())
     {
         // something
+        ListCurrentSources(sourceList);
+        
+        ListCurrentDestinations(destinationList);
     }
     
 }
 
 int main(int argc, const char * argv[])
-{    
+{
+    MIDIClientRef           client;
+    MIDIPortRef             inputPort, outputPort;
+    MIDIEndpointRef         mySource, myDestination;
+    OutputPortDevicePair    myOutputPortDevicePair;
+
+    
     // create MIDI client
-    MIDIClientRef       client;
     MIDIClientCreate(CFSTR("MIDI Middle Man"), NotifyProc, NULL, &client);
     
     // create MIDI source and destination
-    MIDISourceCreate(client, CFSTR("MIDI Middle Man"), &source);
-    MIDIDestinationCreate(client, CFSTR("MIDI Middle Man"), DestinationReadProc, NULL, &destination);
-    
+    MIDISourceCreate(client, CFSTR("MIDI Middle Man"), &mySource);
+    MIDIDestinationCreate(client, CFSTR("MIDI Middle Man"), DestinationReadProc, &myOutputPortDevicePair, &myDestination);
+        
     // create MIDI ports
-    MIDIInputPortCreate(client, CFSTR("MIDI Middle Man"), InputReadProc, NULL , &inputPort);
+    MIDIInputPortCreate(client, CFSTR("MIDI Middle Man"), InputReadProc, &mySource , &inputPort);
     MIDIOutputPortCreate(client, CFSTR("MIDI Middle Man"), &outputPort);
+    myOutputPortDevicePair.outputPort = outputPort;
+
     
     // create an empty array for source list
-    NSMutableArray      *sourceList, *destinationList;
     sourceList = [ NSMutableArray array ];
     destinationList = [ NSMutableArray array];
     
@@ -166,13 +211,17 @@ int main(int argc, const char * argv[])
     gSources = MIDIGetNumberOfSources();
     gDestinations = MIDIGetNumberOfDestinations();
     
-    // ConnectInputs();
-    //  ConnectOutputs();
-    
     CFStringRef desiredSourceName = CFSTR(DESIRED_SOURCE_NAME);
     NSUInteger indexOfDesiredSource = -1;
     ListCurrentSources(sourceList);
     indexOfDesiredSource = FindIndexOfDesiredSource(sourceList, desiredSourceName);
+    ConnectInputs(indexOfDesiredSource, inputPort);
+    
+    CFStringRef desiredDestinationName = CFSTR(DESIRED_DESTINATION_NAME);
+    NSUInteger indexOfDesiredDestination = -1;
+    ListCurrentDestinations(destinationList);
+    indexOfDesiredDestination = FindIndexOfDesiredDestination(destinationList, desiredDestinationName);
+    ConnectOutputs(indexOfDesiredDestination, &myOutputPortDevicePair);
     
     
     CFRunLoopRun();
